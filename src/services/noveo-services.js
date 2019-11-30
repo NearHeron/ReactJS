148ce45906b5a026d3946f64987965291782bc6f
NearHeron <58370847+NearHeron@users.noreply.github.com>
2019-12-01 01:50:08 +0700

    f

---
+++ src/services/noveo-services.js
@@ -0,0 +1,65 @@
+export default class SwapiService {
+
+    _apiBase = 'https://swapi.co/api';
+
+    async getResource(url) {
+        const result = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url} , received ${result.status}`);
+        }
+
+        return await result.json();
+    }
+
+    async getAllPeople() {
+        const result = await this.getResource(`/people/`);
+        return result.results;
+    }
+
+    getPerson(id) {
+        return this.getResource(`/people/${id}`);
+    }
+
+    async getAllPlanets() {
+        const result = await this.getResource(`/planets/`);
+        return result.results;
+    }
+
+    getPlanet(id) {
+        return this.getResource(`/planet/${id}`);
+    }
+
+    async getAllStarships() {
+        const result = await this.getResource(`/starships/`);
+        return result.results;
+    }
+
+    getStarship(id) {
+        return this.getResource(`/starship/${id}`);
+    }
+}
+
+
+// class NoveoTalkService {
+//
+//     _apiBase = 'http://peregovorki-js.noveogroup.com';
+//
+//     async getResource(url) {
+//         const result = await fetch(`${this._apiBase}${url}`)
+//
+//         if(!result.ok) {
+//             throw new Error(`Could not fetch ${url} , received ${result.status}`)
+//         }
+//     }
+//
+//     async getAllRooms() {
+//         return  await this.getResource(`/rooms`)
+//
+//     }
+//
+//     async getRoom(id) {
+//         return this.getResource(`/rooms/${id}`);
+//     }
+//
+// }
